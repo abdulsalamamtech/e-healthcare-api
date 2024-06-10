dc2c4a144fb41498f2cff93d7391f4eb6ac19d04
AbdulsalamAmtech <abdulsalamamtech@gmail.com>
2024-06-10 14:53:47 +0100

    Add request files

--- app/Http/Requests/UpdateMedicalOfficerRequest.php
+++ app/Http/Requests/UpdateMedicalOfficerRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class Updatemedical_officerRequest extends FormRequest
+class UpdateMedicalOfficerRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
